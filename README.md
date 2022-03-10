0f54060afb77188d6cfbc1f7d9aaf298bb88eef7
jddeines <jacob.deines@jacks.sdstate.edu>
2022-03-10 07:34:40 -0600

    Initial Commit
    The app blinks an LED on the pico, writes test strings to the LCD, and
    talks UART back to a terminal through the picoprobe.

---
+++ README.md
@@ -0,0 +1,15 @@
+# pico-clock
+
+Must have the pico-sdk in a directory at the same level as this one!
+
+How to build:
+
+cd build
+cmake -G "NMake Makefiles" ..
+nmake
+
+How to program:
+
+Use this helpful blog post to get set up for programming with a picoprobe:
+
+https://community.element14.com/products/raspberry-pi/b/blog/posts/debugging-the-raspberry-pi-pico-on-windows-10
---
+++ pico-clock.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "pico/binary_info.h"
+
+#define DEFAULT_LCD_ADDR 0x72
+#define DEFAULT_LCD_ROWS 4
+#define DEFAULT_LCD_COLS 20
+
+static const char *resetString = "|-";
+
+static const char *message = "Test 123 ";
+
+void lcd_write_byte(uint8_t val)
+{
+    i2c_write_blocking(i2c_default, DEFAULT_LCD_ADDR, &val, 1, false);
+}
+
+void lcd_string(const char *s)
+{
+    while (*s)
+    {
+        lcd_write_byte(*s++);
+        sleep_us(50);
+    }
+}
+
+int main()
+{
+    stdio_init_all();
+
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+
+    i2c_init(i2c_default, 115200);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+    sleep_ms(1);
+
+    lcd_string(resetString);
+
+    while(1)
+    {
+        gpio_put(25, 1);
+        printf("LED ON!\n");
+        sleep_ms(1000);
+
+        gpio_put(25, 0);
+        printf("LED OFF!\n");
+        sleep_ms(1000);
+
+        lcd_string(message);
+    }
+}
+
+/*
+ Command cheat sheet:
+ ASCII / DEC / HEX
+ '|'    / 124 / 0x7C - Put into setting mode
+ Ctrl+c / 3 / 0x03 - Change width to 20
+ Ctrl+d / 4 / 0x04 - Change width to 16
+ Ctrl+e / 5 / 0x05 - Change lines to 4
+ Ctrl+f / 6 / 0x06 - Change lines to 2
+ Ctrl+g / 7 / 0x07 - Change lines to 1
+ Ctrl+h / 8 / 0x08 - Software reset of the system
+ Ctrl+i / 9 / 0x09 - Enable/disable splash screen
+ Ctrl+j / 10 / 0x0A - Save currently displayed text as splash
+ Ctrl+k / 11 / 0x0B - Change baud to 2400bps
+ Ctrl+l / 12 / 0x0C - Change baud to 4800bps
+ Ctrl+m / 13 / 0x0D - Change baud to 9600bps
+ Ctrl+n / 14 / 0x0E - Change baud to 14400bps
+ Ctrl+o / 15 / 0x0F - Change baud to 19200bps
+ Ctrl+p / 16 / 0x10 - Change baud to 38400bps
+ Ctrl+q / 17 / 0x11 - Change baud to 57600bps
+ Ctrl+r / 18 / 0x12 - Change baud to 115200bps
+ Ctrl+s / 19 / 0x13 - Change baud to 230400bps
+ Ctrl+t / 20 / 0x14 - Change baud to 460800bps
+ Ctrl+u / 21 / 0x15 - Change baud to 921600bps
+ Ctrl+v / 22 / 0x16 - Change baud to 1000000bps
+ Ctrl+w / 23 / 0x17 - Change baud to 1200bps
+ Ctrl+x / 24 / 0x18 - Change the contrast. Follow Ctrl+x with number 0 to 255. 120 is default.
+ Ctrl+y / 25 / 0x19 - Change the TWI address. Follow Ctrl+x with number 0 to 255. 114 (0x72) is default.
+ Ctrl+z / 26 / 0x1A - Enable/disable ignore RX pin on startup (ignore emergency reset)
+ '-'    / 45 / 0x2D - Clear display. Move cursor to home position.
+        / 128-157 / 0x80-0x9D - Set the primary backlight brightness. 128 = Off, 157 = 100%.
+        / 158-187 / 0x9E-0xBB - Set the green backlight brightness. 158 = Off, 187 = 100%.
+        / 188-217 / 0xBC-0xD9 - Set the blue backlight brightness. 188 = Off, 217 = 100%.
+
+ For example, to change the baud rate to 115200 send 124 followed by 18.
+*/
